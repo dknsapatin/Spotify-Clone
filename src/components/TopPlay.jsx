a371e16868fa3dfe32bc024634ba5a3112795e98
Denzel Sapatin <91914302+dknsapatin@users.noreply.github.com>
2022-10-02 23:45:12 -0400

    Adding TopPlay Component

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,5 +1,59 @@
-const TopPlay = () => (
-  <div>TopPlay</div>
-);
+import { useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode } from 'swiper';
+
+import PlayPause from './PlayPause';
+import { playPause, setActiveSong } from '../redux/features/playerSlice';
+import { useGetTopChartsQuery } from '../redux/services/shazamCore';
+
+// TopChartCard Component
+
+const TopPlay = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data } = useGetTopChartsQuery();
+  const divRef = useRef(null);
+
+  // useEffect to default scroll to the top of the page
+  useEffect(() => {
+    divRef.current.scrollIntoView({ behavior: 'smooth' });
+  });
+  // Slice the top 5 data songs
+  const topPlays = data?.slice(0, 5);
+
+  const handlePlayClick = () => {
+    // dispatch() is the verb of the action/ to play or pause the song
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+
+  return (
+    // Top Charts component.. located at the top of the page
+    <div
+      ref={divRef}
+      className='xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col'
+    >
+      <div className='w-full flex flex-col'>
+        <div className='flex flex-row justify-between items-center'>
+          <h2 className='text-white font-bold text-2xl'>Top Charts</h2>
+          <Link to='/top-charts'>
+            <p className='text-gray-300 text-base cursor-pointer'>See more</p>
+          </Link>
+        </div>
+
+        <div className='mt-4 flex flex-col gap-1'>
+          {/* {topPlays.map((song, i) => (
+            <TopChartCard />
+          ))} */}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default TopPlay;
